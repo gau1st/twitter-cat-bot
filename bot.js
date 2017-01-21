e7b56d02c869ea1d11468df2c44daef4321979cf
Gilang Esha Gautama <gau_1st@yahoo.co.id>
2017-01-21 23:19:05 +0700

    fix many things

--- bot.js
+++ bot.js
@@ -1,13 +1,10 @@
 console.log("The BOT is starting");
 
 const Twit = require("twit");
-const exec = require("child_process").exec;
-const fs = require("fs");
-const image_downloader = require('image-downloader');
 const emoji = require('node-emoji');
 const base64 = require('node-base64-image');
-const http = require('http');
 const xmlParseString = require('xml2js').parseString;
+const http = require('http');
 const config = require("./config");
 
 var T = new Twit(config);
@@ -86,72 +83,6 @@ function tweetItWithImageFromUrl(url, text) {
    });
 }
 
-
-//
-//  Function For Post Tweet With Image
-//
-// function tweetItWithImageFromUrl(url, text) {
-//    console.log('Downloading Image...');
-//    // Download to a directory and save with an another filename
-//    var options = {
-//        url: url,
-//        dest: 'images/image.jpg',
-//        done: function(err, filename, image) {
-//            if (err) {
-//              console.log(err);
-//            } else {
-//              console.log('File saved to', filename);
-//              tweetItWithImage('images/image.jpg', text);
-//            }
-//        },
-//    };
-//    image_downloader(options);
-// }
-
-// var params = {
-//    id : 23424846
-// }
-
-//
-//  Function For Post Tweet
-//
-// function tweetIt(text) {
-//    var r = Math.floor(Math.random()*100);
-//    T.post('statuses/update', { status: text + " -- : " + r }, function(err, data, response) {
-//       if (err) {
-//          console.log("Something Went Wrong...");
-//       } else {
-//          console.log("Posted!!!")
-//       }
-//    })
-// }
-
-// //
-// //  Function For Post Tweet With Trending
-// //
-// function tweetItWithTrending(text) {
-//    var r = Math.floor(Math.random()*100);
-//    //
-//    //  search twitter for all tweets containing the word 'banana' since July 11, 2011
-//    //
-//    T.get('trends/place', params, function(err, data, response) {
-//       var trending = data[0].trends;
-//       for (var i = 0; i < trending.length; i++) {
-//          console.log("--- "+trending[i].name);
-//       }
-//
-//       var trendRandom = Math.floor(Math.random()*10);
-//       tweetIt(text + " " + trending[trendRandom].name);
-//    })
-// }
-//
-// //
-// // Function to generate random integer in between
-// //
-// function getRandomInt(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
 //  Setting up a user stream
 var stream = T.stream('user')
 
@@ -160,26 +91,27 @@ stream.on('follow', function (event) {
    var name = event.source.name;
    var screenName = event.source.screen_name;
    if (screenName!="kitten_evriday") {
-      tweetItWithImageFromUrl('https://lh6.ggpht.com/sw_iT7GZASdAYeiecsZEHJE-EgDhdK2rCWUzZOJS0OFiGpoi9qn8iMH2nuXHgWg2PA=h900', ".@"+screenName + " Hi, "+name+" Thx for following me.. "+ emoji.emojify(':heart: :heart:'));
+      tweetItWithImageFromUrl('https://lh6.ggpht.com/sw_iT7GZASdAYeiecsZEHJE-EgDhdK2rCWUzZOJS0OFiGpoi9qn8iMH2nuXHgWg2PA=h900', ".@"+screenName + " Hi, "+name+", Thx for following me.. "+ emoji.emojify(':heart: :heart:'));
    }
 });
 
 
 setInterval(function() {
    xmlToJson(urlXml, function(err, data) {
-     if (err) {
-       // Handle this however you like
-       return console.err(err);
-     }
+      if (err) {
+         // Handle this however you like
+         return console.err(err);
+      }
 
-     // Do whatever you want with the data here
-     // Following just pretty-prints the object
-     var dataConverted = JSON.stringify(data, null, 2);
-     var JsonDataconverted = JSON.parse(dataConverted);
-     var imageUrl = JsonDataconverted.response.data[0].images[0].image[0].url[0];
-     var sourceUrl = JsonDataconverted.response.data[0].images[0].image[0].source_url[0];
+      // Do whatever you want with the data here
+      // Following just pretty-prints the object
+      var dataConverted = JSON.stringify(data, null, 2);
+      var JsonDataconverted = JSON.parse(dataConverted);
+      var imageUrl = JsonDataconverted.response.data[0].images[0].image[0].url[0];
+      var sourceUrl = JsonDataconverted.response.data[0].images[0].image[0].source_url[0];
 
-     tweetItWithImageFromUrl(imageUrl, "Miaww Miaww..."+ emoji.emojify(':heart: :heart:')+"\n\n\n\n\nsource : "+imageUrl);
+      tweetItWithImageFromUrl(imageUrl, "Miaww Miaww..."+ emoji.emojify(':heart: :heart:')+"\n\n\n\n\nsource : "+imageUrl);
+
    });
 
 }, 1000*900);
